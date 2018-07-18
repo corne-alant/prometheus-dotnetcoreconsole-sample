785c70be018e53a3f3ba6f4ad37b9ed89e51bda4
Corné Alant <corne.alant@outlook.com>
2018-07-18 15:00:55 +1200

    metric server code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using Prometheus;
+
+namespace prometheus_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            //start prom server
+            var metricServer = new KestrelMetricServer(port: 1234);
+            metricServer.Start();
+
+            Thread.Sleep(200000);
+
+            Console.WriteLine("Exit");
+        }
+    }
+}
